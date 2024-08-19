fe795c0235eb97df7380d2512f21e904d275b0f9
Emmanuella Orioma <oriomaemmanuella@gmail.com>
2024-08-20 00:44:27 +0100

    Normalize line endings

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_ya5kGhD10vYlilV0E8q2trhmfbZI0ow",
+  authDomain: "flashcardsaas-11743.firebaseapp.com",
+  projectId: "flashcardsaas-11743",
+  storageBucket: "flashcardsaas-11743.appspot.com",
+  messagingSenderId: "1092526268505",
+  appId: "1:1092526268505:web:acbea9c57e828e85c21148"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
